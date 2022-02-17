384623eccf6e8e5378dcb7013333585520488be1
Moule MohanRaj S <85351172+Moule-2419@users.noreply.github.com>
2022-02-17 20:22:18 +0530

    ALPHABET.c
    https://www.codechef.com/problems/ALPHABET

---
+++ ALPHABET.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include <string.h>
+char str[27];
+int len=0;
+int main()
+{
+    scanf("%s",str);
+    len=strlen(str);
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        char str1[27];
+        int count=0;
+        scanf("%s",&str1);
+        for(int i=0;str[i]!='\0';i++)
+        {
+            int j=0;
+            while(str1[j]!='\0')
+            {
+                if(str[i]==str1[j])
+                {
+                    count++;
+                    break;
+                }
+                j++;
+            }
+        }
+        if(count==len)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
